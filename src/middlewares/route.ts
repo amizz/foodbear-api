72a92ecf1dadd8778133c77be51c7dc565bb3609
Amirul Zharfan Zalid <am.zharfan@gmail.com>
2022-08-10 23:55:37 +0800

    restaurant route

--- src/middlewares/route.ts
+++ src/middlewares/route.ts
@@ -1,9 +1,11 @@
 import { Fastify, Req, Res } from '#interfaces/fastify'
 import { mainRoute } from '#routes/main';
+import { restaurantRoute } from '#routes/restaurant';
 
 export const routeMiddleware = (app: Fastify): void => {
   /**
    * @description all route initialize here
    */
   app.register(mainRoute, { prefix: '/' });
+  app.register(restaurantRoute);
 }
