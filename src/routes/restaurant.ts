e501c6cadb7caad831d86cc24649715271ac1296
Amirul Zharfan Zalid <am.zharfan@gmail.com>
2022-08-11 11:26:38 +0800

    Top and all restaurants route

--- src/routes/restaurant.ts
+++ src/routes/restaurant.ts
@@ -1,9 +1,11 @@
+import { allRestaurant } from '#controllers/restaurant/all-restaurants';
 import { searchRestaurant, searchRestaurantSchema } from '#controllers/restaurant/search-restaurant';
 import { topRestaurant } from '#controllers/restaurant/top-restaurant';
 import { Ctx, Done, Fastify, Req, Res, RouteOptions } from '../interfaces/fastify'
 
 export const restaurantRoute = (route: Fastify, options: RouteOptions, done: Done): any => {
 
+  route.get('/restaurants', allRestaurant);
   route.get('/restaurants/search', searchRestaurant);
   route.get('/restaurants/top', topRestaurant);
 
